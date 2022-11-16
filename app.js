336db46c4da8e10ed443a2a70f03948da1d3f475
NoelGamaliel <techmbala@gmail.com>
2022-11-16 11:45:54 +0100

    My first commit of the exercice Js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// My page of the script
+// My page of the script;
+
+
+// const container = document.querySelector('div');
+
+// console.log(container);
+
+
+// const count = document.getElementById('count');
+
+// console.log(count);
+
+
+const count = document.querySelector('#count').style.backgroundColor= "red";
+const elt = document.querySelector('#count').style.fontSize = "2rem";
+
+console.log(count);
+
+console.log(elt);
+
+
+const myFristVariable = document.getElementById('myFristVariable');
+myFristVariable.classList.add("newClass")
+
+const newClass = document.querySelector('.newClass').style.border= "2px solid #ddd";
+
+console.log(myFristVariable);
